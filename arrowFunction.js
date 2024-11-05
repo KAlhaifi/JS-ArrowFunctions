fed2bb14aebcbeae392562641640efb8839252e9
khalaf kha <khskh.alh@gmail.com>
2024-11-05 08:17:28 +0300

    task completed

---
+++ arrowFunction.js
@@ -0,0 +1,11 @@
+let greet = (name) => `hello ${name}`;
+
+console.log(greet("Khalaf"));
+
+let sum = (fNum, sNum) => fNum + sNum;
+
+console.log(sum(5, 3));
+
+let squaers = (num1) => num1 ** 2;
+
+console.log(squaers(2));
